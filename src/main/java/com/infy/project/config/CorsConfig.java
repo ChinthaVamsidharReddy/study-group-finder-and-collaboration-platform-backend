02ff2b6eddf34ea4936aee40547296f28bc2b85c
Chintha Vamsidhar Reddy <161109171+ChinthaVamsidharReddy@users.noreply.github.com>
2025-11-15 13:03:40 +0530

    Update CorsConfig.java

--- src/main/java/com/infy/project/config/CorsConfig.java
+++ src/main/java/com/infy/project/config/CorsConfig.java
@@ -1,24 +1,24 @@
-// package com.infy.project.config;
+package com.infy.project.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("https://study-group-finder-and-collaboratio.vercel.app")
-// //                		.allowedOrigins("**")
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                         .allowedHeaders("*")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://study-group-finder-and-collaboratio.vercel.app")
+//                		.allowedOrigins("**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
